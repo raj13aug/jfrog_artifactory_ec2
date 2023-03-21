8bc6db1788d403c4bf99f19eefdcf4305cce9575
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-21 21:19:26 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "ec2" {
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8082
+    to_port     = 8082
     protocol    = "tcp"
   }
   egress {
